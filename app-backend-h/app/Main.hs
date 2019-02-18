484906775939af5eb1728cd8b982918016c419a1
James Santucci <james.santucci@gmail.com>
2019-02-17 21:18:57 -0500

    Remove old app code that just printed things to prove they worked

--- app-backend-h/app/Main.hs
+++ app-backend-h/app/Main.hs
@@ -24,27 +24,4 @@ someOrFilters = [ Filter.orFilter "owner = 'a bogus user who does not exist'"
 
 main :: IO ()
 main = do
-  conn <- Postgres.connectPostgreSQL connString
-  user <- getUser
-  handle <- pure $ Database.Handle conn
-  result <- createProject handle Project.testProject user
-  fetchResult <- getProject handle (Project.id . head $ result)
-  recentlyCheckedAOIs <- checkAOIs . head $ fetchResult
-  updateResult <- updateProject handle recentlyCheckedAOIs user
-  fetchAfterUpdate <- getProject handle (Project.id recentlyCheckedAOIs)
-  print "And filters:"
-  print $ Filter.toQuery . Filter.toFilter . mconcat $ someAndFilters
-  andListResults <- listProjects handle (mconcat someAndFilters)
-  print "Or filters:"
-  print $ Filter.toQuery . Filter.toFilter . mconcat $ someOrFilters
-  orListResults <- listProjects handle (mconcat someOrFilters)
-  print "Result of project creation:"
-  print result
-  print "Result of fetching project by id after creation"
-  print fetchResult
-  print "Result of listing after and-ing two filters:"
-  print andListResults
-  print "Result of listing after or-ing two filters:"
-  print orListResults
-  print "Result of fetch after updating aois last checked:"
-  print fetchAfterUpdate
+  print "you're doing great"
